838047d4dd85ee1f712e62a49ebbad627188c4ad
T.J. Mazeika <bionicrm@gmail.com>
2017-02-04 19:42:34 -0500

    Work on web socket bridge

--- main.go
+++ main.go
@@ -2,26 +2,83 @@ package main
 
 import (
 	"net/http"
-	"io"
-	"encoding/json"
 	"log"
+	"github.com/gorilla/websocket"
 )
 
+type webWsHandler func(wsData)
+
+var tokenHandlerMap = map[string]func(wsData){}
+
+type wsData struct {
+	X            float64 `json:"x"`
+	Y            float64 `json:"y"`
+	Z            float64 `json:"z"`
+	TappedX      bool    `json:"tapped_x"`
+	TappedY      bool    `json:"tapped_y"`
+	TappedZ      bool    `json:"tapped_z"`
+	TopButton    bool    `json:"top_button"`
+	MiddleButton bool    `json:"middle_button"`
+	BottomButton bool    `json:"bottom_button"`
+	LeftButton   bool    `json:"left_button"`
+}
+
 func main() {
-	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/ws/pebble", pebbleHandler)
+	http.HandleFunc("/ws/web", webHandler)
 	if err := http.ListenAndServe("0.0.0.0:8080", nil); err != nil {
 		log.Fatalln(err)
 	}
 }
 
-type homeJson struct {
-	SomeKey string `json:"some_key"`
-}
-
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	b, err := json.Marshal(homeJson{"some value"})
+func pebbleHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Println(err)
+		return
+	}
+	token := r.Form.Get("token")
+	ws, err := websocket.Upgrader{}.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println(err)
+		return
 	}
-	io.WriteString(w, string(b))
+	defer ws.Close()
+
+	for {
+		data := wsData{}
+		if err := ws.ReadJSON(data); err != nil {
+			log.Println(err)
+			continue
+		}
+		if handler, ok := tokenHandlerMap[token]; ok {
+			handler(data)
+		}
+	}
+}
+
+func webHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		log.Println(err)
+		return
+	}
+	token := r.Form.Get("token")
+	ws, err := websocket.Upgrader{}.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer ws.Close()
+
+	for {
+		data := wsData{}
+		if err := ws.ReadJSON(data); err != nil {
+			log.Println(err)
+			continue
+		}
+		tokenHandlerMap[token] = func(data wsData) {
+			if err := ws.WriteJSON(data); err != nil {
+				log.Println(err)
+			}
+		}
+	}
 }
